1e0d79e34fe4c44463b03b3c08afd77e52769923
Devin Collins <me@imdevinc.com>
2022-08-28 17:32:45 -0700

    Added new script for updating movie titles automatically

---
+++ add_movie.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+MOVIE_NAME=$1
+TEMP_URL=$2
+
+expected_content_type="image/jpeg"
+content_type=$(curl -s -I -o /dev/null -w '%{content_type}' ${TEMP_URL})
+
+check_url=$(echo ${TEMP_URL} | grep "performed_by")
+if [ ! -z ${check_url} ]; then
+	TEMP_URL=$(echo ${TEMP_URL} | sed 's/\([0-9]\+\)\/download.*/\1/')
+fi
+
+MOVIE_NAME=$(echo ${MOVIE_NAME} | sed 's/^Missing poster for movie //')
+
+if [ "${content_type}" != "${expected_content_type}" ]; then
+	echo "Invalid content type returned for URL ${TEMP_URL}: \`${content_type}\`"
+	exit 1
+fi
+
+temp_url=${TEMP_URL} metadata_path=".metadata.[\"${MOVIE_NAME}\"].url_poster" yq -i 'eval(strenv(metadata_path)) = strenv(temp_url)' config/movies.yml
